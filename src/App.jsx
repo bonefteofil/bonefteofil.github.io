4db4620529199cc9329159e0175629ed32e8d09f
Teofil <bonef.teofil@gmail.com>
2025-03-03 22:57:41 +0200

    small ui repairs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-import './App.css'
 import ButtonProject from '../components/button_project'
 
 function App() {
@@ -8,11 +6,10 @@ function App() {
     <>
       <h1>Bonef Teofil's projects</h1>
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '20px' }}>
-        <ButtonProject href="https://bonefteofil.github.io/Atestat-Info" name="Atestat Info" />
-        <ButtonProject href="https://bonefteofil.github.io/LogIn/login.html" name="Login" />
-        <ButtonProject href="https://bonefteofil.github.io/xand0" name="X and 0 Game" />
-        <ButtonProject href="https://bonefteofil.github.io/Teofil-Projects-Old" name="Old projects" />
-
+        <ButtonProject href="https://bonefteofil.github.io/xand0" name="X and 0 Game 2023" />
+        <ButtonProject href="https://bonefteofil.github.io/LogIn/login.html" name="Simple auth design 2022" />
+        <ButtonProject href="https://bonefteofil.github.io/Atestat-Info" name="Certificate info 2022" />
+        <ButtonProject href="https://bonefteofil.github.io/Teofil-Projects-Old" name="Old projects 2019" />
       </div>
     </>
   )

f95563bca38a3afc6f9ecf2fdf4cf768a522c25d
Teofil <bonef.teofil@gmail.com>
2025-03-25 16:58:51 +0200

    added github link to 2019 C# game engine

--- content/projects/game-engine-cSharp.tsx
+++ content/projects/game-engine-cSharp.tsx
@@ -4,9 +4,11 @@ export default function GameEngineCSharp() {
 
     return (
         <ButtonProject
-            title='2019 - 2D Game Engine for Console in C#'
+            title='2019 - 2D Game Engine for Console in C-Sharp'
+            github="https://github.com/bonefteofil/TeoEngine"
+            images={3}
         >
-            I made a library that renders polygons, ellipses, text, and buttons with properties such as
+            I made a library that displays polygons, ellipses, text, and buttons with properties such as
             position, rotation, color, parent-child relationships, and mouse events. It also
             provides functions for a timer, mouse control, FPS adjustment, and console dimensions.
         </ButtonProject>
